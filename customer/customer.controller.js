e77c9dde6b57e4e20cb345bac699108086d4b8c9
neupanesaugat <saugatneupane50@gmail.com>
2024-07-11 16:27:22 +0545

    Foodmandu APIs using express js

--- customer/customer.controller.js
+++ customer/customer.controller.js
@@ -43,4 +43,73 @@ router.get("/customer/detail/:id", async (req, res) => {
   //? send res
   return res.status(200).send({ message: "Success", customerDetail: customer });
 });
+
+//? delete a customer by id
+router.delete("/customer/delete/:id", async (req, res) => {
+  //? extract customer id from req.params
+  const customerId = req.params.id;
+
+  //? check for mongo id validity
+  const isValidId = mongoose.isValidObjectId(customerId);
+
+  //? if not valid mongo id, throw error
+  if (!isValidId) {
+    return res.status(400).send({ message: "Invalid Mongo ID..." });
+  }
+
+  //? find customer using customer id
+
+  // const customer = await Customer.findOne({ _id: customerId });
+  const customer = await Customer.findById(customerId); //? alternative by mongoose
+
+  //? if not customer, throw error
+  if (!customer) {
+    return res.status(404).send({ message: "Customer does not exist..." });
+  }
+
+  //? delete customer
+  // await Customer.deleteOne({ _id: customerId });
+  await Customer.findByIdAndDelete(customerId); //? alternative by mongoose
+
+  //? send res
+  return res
+    .status(200)
+    .send({ message: "Customer is deleted successfully..." });
+});
+
+//? edit customer by id
+router.put("/customer/edit/:id", async (req, res) => {
+  //? extract customer id from req.params
+  const customerId = req.params.id;
+
+  //? check for mongo id validity
+  const isValidId = mongoose.isValidObjectId(customerId);
+
+  //? if not valid mongo id, throw error
+  if (!isValidId) {
+    return res.status(400).send({ message: "Invalid Id..." });
+  }
+
+  //? find customer
+  const customer = await Customer.findById(customerId);
+
+  //? if not customer, throw error
+  if (!customer) {
+    return res.status(404).send({ message: "Customer does not exist..." });
+  }
+
+  // extract new values from req.body
+  const newValues = req.body;
+
+  //? update customer
+  // await Customer.updateOne({
+  //   ...newValues, //? spreading values from new values rather than rewriting all the editing values
+  // });
+
+  await Customer.findByIdAndUpdate(customerId, { ...newValues }); //? alternative by mongoose
+
+  //? send res
+  return res.status(200).send({ message: "Editing..." });
+});
+
 export default router;
